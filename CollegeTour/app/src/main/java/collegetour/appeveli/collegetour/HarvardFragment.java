f8fbc60b04b0f868fae228f1c5748b358bc02f86
Dominque Terry <terry_dominque@yahoo.com>
2015-01-11 19:23:10 -0800

    College Tour
    This is an Android application that shows different colleges in the
    world.  This project uses Google maps API.

---
+++ CollegeTour/app/src/main/java/collegetour/appeveli/collegetour/HarvardFragment.java
@@ -0,0 +1,10 @@
+package collegetour.appeveli.collegetour;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+/**
+ * Created by Dterryable on 11/1/2014.
+ */
+public class HarvardFragment {
+
+}
